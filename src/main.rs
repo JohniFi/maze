c3b8edb5cda2cd6c47b38606df07473d8209914a
JohniFi <25084862+JohniFi@users.noreply.github.com>
2024-08-15 18:51:31 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,81 @@
+#[derive(Debug)]
+struct Maze {
+    map: Vec<Vec<bool>>, // false represents walls, true represents floor
+    width: usize,
+    height: usize,
+    start_x: usize,
+    start_y: usize,
+    
+    visited: Vec<Vec<bool>>,
+    path_map: Vec<Vec<bool>>,
+}
+
+impl Maze {
+    // Character for walls: 'X'
+    const INPUT_WALL: char = 'X';
+    // Character for floor: ' '
+    const INPUT_FLOOR: char = ' ';
+
+    const OUTPUT_WALL: char = '⬜';
+    const OUTPUT_FLOOR: char = '⬛';
+    const OUTPUT_START: char = '❌';
+    const OUTPUT_PATH: char = '◽';
+
+    /// Creates a new [`Maze`].
+
+    fn new(map: Vec<Vec<bool>>, start_x: usize, start_y: usize) -> Result<Maze, String> {
+        let width = map.iter().map(|row| row.len()).max().unwrap_or_default();
+
+        let height = map.len();
+
+        if height < 3 || width < 3 {
+            return Err("Maze is too small. Minimum 3x3".to_string());
+        }
+
+        let mut map = map.to_vec();
+
+        // make sure all rows are the same length
+        for row in map.iter_mut() {
+            row.resize(width, false)
+        }
+
+
+        if let Some(row_vec) = map.get(start_y) {
+            if let Some(&value) = row_vec.get(start_x) {
+                if !value {
+                    return Err("Starting position must not be on a wall!".to_string());
+                }
+            } else {
+                return Err("start_x out of bounds!".to_string());
+            }
+        } else {
+            return Err("start_y out of bounds!".to_string());
+        }
+
+        Ok(Self {
+            map, // false represents walls, true represents floor
+            width,
+            height,
+            start_x,
+            start_y,
+            
+            visited: vec![vec![false; width]; height],
+            path_map: vec![vec![false; width]; height],
+        })
+        
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    let original: Vec<Vec<bool>> = vec![
+        vec![true, false, true],
+        vec![false, true, false],
+        vec![true, true, false],
+    ];
+
+    let test_maze = Maze::new(original, 1, 1);
+
+    println!("test_maze: {test_maze:?}");
+}
