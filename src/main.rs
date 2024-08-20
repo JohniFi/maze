345ee1f2a9532029488877550454de4a1c7f15c0
JohniFi <25084862+JohniFi@users.noreply.github.com>
2024-08-20 14:30:33 +0200

    add encapsulation via module

--- src/main.rs
+++ src/main.rs
@@ -1,229 +1,242 @@
-use std::fmt;
+mod maze {
+    use std::fmt;
 
-pub trait AsChar {
-    fn as_char(&self) -> char;
-}
-
-#[derive(Clone, Debug, Default, PartialEq)]
-enum MazeCell {
-    #[default]
-    Wall,
-    Floor(FloorType),
-}
-
-impl AsChar for MazeCell {
-    fn as_char(&self) -> char {
-        match self {
-            MazeCell::Wall => '⬜',
-            MazeCell::Floor(f) => f.as_char(),
-        }
+    pub trait AsChar {
+        fn as_char(&self) -> char;
     }
-}
 
-impl MazeCell {
-    fn from_bool(value: bool) -> Self {
-        if value {
-            MazeCell::Floor(FloorType::default())
-        } else {
-            MazeCell::default()
-        }
+    #[derive(Clone, Debug, Default, PartialEq)]
+    pub enum MazeCell {
+        #[default]
+        Wall,
+        Floor(FloorType),
     }
-}
 
-#[derive(Clone, Debug, Default, PartialEq)]
-enum FloorType {
-    #[default]
-    Floor,
-    Start,
-    Path,
-}
-
-impl AsChar for FloorType {
-    fn as_char(&self) -> char {
-        match self {
-            FloorType::Floor => '⬛',
-            FloorType::Start => '❌',
-            FloorType::Path => '👣',
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Maze {
-    /// `false` represents walls, `true` represents floor
-    map: Vec<Vec<MazeCell>>, // false represents walls, true represents floor
-    width: usize,
-    height: usize,
-    start_x: usize,
-    start_y: usize,
-
-    visited: Option<Vec<Vec<bool>>>,
-}
-
-trait Solvable {
-    fn solve(&mut self) -> Result<bool, String>;
-
-    fn solve_from(&mut self, x: usize, y: usize) -> Result<bool, String>;
-}
-
-impl Maze {
-    /// Character for walls: 'X'
-    const INPUT_WALL: char = 'X';
-    /// Character for floor: ' '
-    const INPUT_FLOOR: char = ' ';
-
-    /// Creates a new [`Maze`].
-    fn new(mut map: Vec<Vec<MazeCell>>, start_x: usize, start_y: usize) -> Result<Maze, String> {
-        let height = map.len();
-        let width = map.iter().map(|row| row.len()).max().unwrap_or_default();
-
-        if height < 3 || width < 3 {
-            return Err("Maze is too small. Minimum 3x3".to_string());
-        }
-
-        // make sure all rows are the same length
-        for row in map.iter_mut() {
-            // fill shorter rows with default MazeCell (Wall)
-            row.resize(width, MazeCell::default())
-        }
-
-        if let Some(row_vec) = map.get(start_y) {
-            if let Some(value) = row_vec.get(start_x) {
-                if value == &MazeCell::Wall {
-                    return Err("Starting position must not be on a wall!".to_string());
-                }
-            } else {
-                return Err("start_x out of bounds!".to_string());
+    impl AsChar for MazeCell {
+        fn as_char(&self) -> char {
+            match self {
+                MazeCell::Wall => '⬜',
+                MazeCell::Floor(f) => f.as_char(),
             }
-        } else {
-            return Err("start_y out of bounds!".to_string());
         }
-
-        Ok(Self {
-            map,
-            width,
-            height,
-            start_x,
-            start_y,
-            visited: None,
-        })
     }
 
-    fn new_from_bool_array(
-        map: Vec<Vec<bool>>,
+    impl MazeCell {
+        fn from_bool(value: bool) -> Self {
+            if value {
+                MazeCell::Floor(FloorType::default())
+            } else {
+                MazeCell::default()
+            }
+        }
+    }
+
+    #[derive(Clone, Debug, Default, PartialEq)]
+    pub enum FloorType {
+        #[default]
+        Floor,
+        Start,
+        Path,
+    }
+
+    impl AsChar for FloorType {
+        fn as_char(&self) -> char {
+            match self {
+                FloorType::Floor => '⬛',
+                FloorType::Start => '❌',
+                FloorType::Path => '👣',
+            }
+        }
+    }
+
+    #[derive(Debug)]
+    pub struct Maze {
+        /// `false` represents walls, `true` represents floor
+        map: Vec<Vec<MazeCell>>, // false represents walls, true represents floor
+        width: usize,
+        height: usize,
         start_x: usize,
         start_y: usize,
-    ) -> Result<Maze, String> {
-        let new_map = map
-            .into_iter()
-            .map(|row| row.into_iter().map(MazeCell::from_bool).collect())
-            .collect();
-        Maze::new(new_map, start_x, start_y)
+
+        visited: Option<Vec<Vec<bool>>>,
     }
 
-    fn new_from_str_array(map: Vec<&str>, start_x: usize, start_y: usize) -> Result<Maze, String> {
-        let grid: Result<Vec<Vec<MazeCell>>, String> = map
-            .iter()
-            .map(|&row| {
-                row.chars()
-                    .map(|c| match c {
-                        Maze::INPUT_FLOOR => Ok(MazeCell::Floor(FloorType::default())),
-                        Maze::INPUT_WALL => Ok(MazeCell::Wall),
-                        _ => Err(format!("Unknown character '{}' in provided maze data!", c)),
-                    })
-                    .collect::<Result<Vec<MazeCell>, String>>()
-            })
-            .collect();
+    pub trait Solvable {
+        fn solve(&mut self) -> Result<bool, String>;
 
-        // propagate any errors
-        let grid = grid?;
-
-        Maze::new(grid, start_x, start_y)
+        fn solve_from(&mut self, x: usize, y: usize) -> Result<bool, String>;
     }
 
-    fn new_from_str(map: &str, start_x: usize, start_y: usize) -> Result<Maze, String> {
-        let array_map = map.split('\n').collect::<Vec<&str>>();
-        Maze::new_from_str_array(array_map, start_x, start_y)
-    }
-}
+    impl Maze {
+        /// Character for walls: 'X'
+        const INPUT_WALL: char = 'X';
+        /// Character for floor: ' '
+        const INPUT_FLOOR: char = ' ';
 
-impl fmt::Display for Maze {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut s = String::new();
+        /// Creates a new [`Maze`].
+        pub fn new(
+            mut map: Vec<Vec<MazeCell>>,
+            start_x: usize,
+            start_y: usize,
+        ) -> Result<Maze, String> {
+            let height = map.len();
+            let width = map.iter().map(|row| row.len()).max().unwrap_or_default();
 
-        for (y, row) in self.map.iter().enumerate() {
-            for (x, cell) in row.iter().enumerate() {
-                if x == self.start_x && y == self.start_y {
-                    // start position
-                    s.push(FloorType::Start.as_char())
-                } else {
-                    s.push(cell.as_char())
-                }
+            if height < 3 || width < 3 {
+                return Err("Maze is too small. Minimum 3x3".to_string());
             }
-            s.push('\n');
+
+            // make sure all rows are the same length
+            for row in map.iter_mut() {
+                // fill shorter rows with default MazeCell (Wall)
+                row.resize(width, MazeCell::default())
+            }
+
+            if let Some(row_vec) = map.get(start_y) {
+                if let Some(value) = row_vec.get(start_x) {
+                    if value == &MazeCell::Wall {
+                        return Err("Starting position must not be on a wall!".to_string());
+                    }
+                } else {
+                    return Err("start_x out of bounds!".to_string());
+                }
+            } else {
+                return Err("start_y out of bounds!".to_string());
+            }
+
+            Ok(Self {
+                map,
+                width,
+                height,
+                start_x,
+                start_y,
+                visited: None,
+            })
         }
 
-        write!(f, "{}", s)
+        pub fn new_from_bool_array(
+            map: Vec<Vec<bool>>,
+            start_x: usize,
+            start_y: usize,
+        ) -> Result<Maze, String> {
+            let new_map = map
+                .into_iter()
+                .map(|row| row.into_iter().map(MazeCell::from_bool).collect())
+                .collect();
+            Maze::new(new_map, start_x, start_y)
+        }
+
+        pub fn new_from_str_array(
+            map: Vec<&str>,
+            start_x: usize,
+            start_y: usize,
+        ) -> Result<Maze, String> {
+            let grid: Result<Vec<Vec<MazeCell>>, String> = map
+                .iter()
+                .map(|&row| {
+                    row.chars()
+                        .map(|c| match c {
+                            Maze::INPUT_FLOOR => Ok(MazeCell::Floor(FloorType::default())),
+                            Maze::INPUT_WALL => Ok(MazeCell::Wall),
+                            _ => Err(format!("Unknown character '{}' in provided maze data!", c)),
+                        })
+                        .collect::<Result<Vec<MazeCell>, String>>()
+                })
+                .collect();
+
+            // propagate any errors
+            let grid = grid?;
+
+            Maze::new(grid, start_x, start_y)
+        }
+
+        pub fn new_from_str(map: &str, start_x: usize, start_y: usize) -> Result<Maze, String> {
+            let array_map = map.split('\n').collect::<Vec<&str>>();
+            Maze::new_from_str_array(array_map, start_x, start_y)
+        }
     }
-}
 
-impl Solvable for Maze {
-    fn solve(&mut self) -> Result<bool, String> {
-        self.visited = Some(vec![vec![false; self.width]; self.height]);
-        self.solve_from(self.start_x, self.start_y)
-    }
+    impl fmt::Display for Maze {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            let mut s = String::new();
 
-    fn solve_from(&mut self, x: usize, y: usize) -> Result<bool, String> {
-        if let (Some(cell), Some(visited)) = (
-            self.map.get_mut(y).and_then(|row| row.get_mut(x)),
-            self.visited
-                .as_mut()
-                .and_then(|v| v.get_mut(y).and_then(|row| row.get_mut(x))),
-        ) {
-            if cell == &MazeCell::Wall {
-                // on wall
-                return Ok(false);
-            }
-
-            if *visited {
-                // already visited
-                return Ok(false);
-            }
-
-            *visited = true;
-
-            if x == 0 || x >= self.width - 1 || y == 0 || y >= self.height - 1 {
-                // found edge (finish)
-                *cell = MazeCell::Floor(FloorType::Path);
-                return Ok(true);
-            }
-
-            // Try to solve from neighboring positions
-            for (next_x, next_y) in [
-                (x.wrapping_sub(1), y),
-                (x + 1, y),
-                (x, y.wrapping_sub(1)),
-                (x, y + 1),
-            ] {
-                if self.solve_from(next_x, next_y)? {
-                    //*cell = MazeCell::Floor(FloorType::Path);  // NOTE: here not possible because of borrow checker
-
-                    if let Some(cell) = self.map.get_mut(y).and_then(|row| row.get_mut(x)) {
-                        *cell = MazeCell::Floor(FloorType::Path);
-                        return Ok(true);
+            for (y, row) in self.map.iter().enumerate() {
+                for (x, cell) in row.iter().enumerate() {
+                    if x == self.start_x && y == self.start_y {
+                        // start position
+                        s.push(FloorType::Start.as_char())
                     } else {
-                        return Err(format!("Starting position ({}, {}) out of bounds", x, y));
+                        s.push(cell.as_char())
                     }
                 }
+                s.push('\n');
             }
-            Ok(false)
-        } else {
-            Err(format!("Starting position ({}, {}) out of bounds", x, y))
+
+            write!(f, "{}", s)
+        }
+    }
+
+    impl Solvable for Maze {
+        fn solve(&mut self) -> Result<bool, String> {
+            self.visited = Some(vec![vec![false; self.width]; self.height]);
+            self.solve_from(self.start_x, self.start_y)
+        }
+
+        fn solve_from(&mut self, x: usize, y: usize) -> Result<bool, String> {
+            if let (Some(cell), Some(visited)) = (
+                self.map.get_mut(y).and_then(|row| row.get_mut(x)),
+                self.visited
+                    .as_mut()
+                    .and_then(|v| v.get_mut(y).and_then(|row| row.get_mut(x))),
+            ) {
+                if cell == &MazeCell::Wall {
+                    // on wall
+                    return Ok(false);
+                }
+
+                if *visited {
+                    // already visited
+                    return Ok(false);
+                }
+
+                *visited = true;
+
+                if x == 0 || x >= self.width - 1 || y == 0 || y >= self.height - 1 {
+                    // found edge (finish)
+                    *cell = MazeCell::Floor(FloorType::Path);
+                    return Ok(true);
+                }
+
+                // Try to solve from neighboring positions
+                for (next_x, next_y) in [
+                    (x.wrapping_sub(1), y),
+                    (x + 1, y),
+                    (x, y.wrapping_sub(1)),
+                    (x, y + 1),
+                ] {
+                    if self.solve_from(next_x, next_y)? {
+                        //*cell = MazeCell::Floor(FloorType::Path);  // NOTE: here not possible because of borrow checker
+
+                        if let Some(cell) = self.map.get_mut(y).and_then(|row| row.get_mut(x)) {
+                            *cell = MazeCell::Floor(FloorType::Path);
+                            return Ok(true);
+                        } else {
+                            return Err(format!("Starting position ({}, {}) out of bounds", x, y));
+                        }
+                    }
+                }
+                Ok(false)
+            } else {
+                Err(format!("Starting position ({}, {}) out of bounds", x, y))
+            }
         }
     }
 }
 
 fn main() {
+    use maze::Maze;
+    use maze::Solvable;
+
     let mut mazes = Vec::new();
 
     mazes.push(
